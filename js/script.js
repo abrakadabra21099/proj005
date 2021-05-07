b590e4f05f6e93dfd7169434d15d5da80fa34045
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-05-07 16:39:02 +0400

    Learn 68. Done.

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 "use strict";
 
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
 
-console.log(typeof(5 + ''));
+// console.log(typeof(5 + ''));
 
-const num = 6;
+// const num = 6;
 
-console.log('https://vk..com/catalog/' + num);
+// console.log('https://vk..com/catalog/' + num);
 
-const fontSize = 26 + 'px';
+// const fontSize = 26 + 'px';
 
-// toNumber
-console.log(typeof(Number('4')));
-console.log(typeof(+'4'));
-console.log(typeof( parseInt(fontSize, 10) ));
+// // toNumber
+// console.log(typeof(Number('4')));
+// console.log(typeof(+'4'));
+// console.log(typeof( parseInt(fontSize, 10) ));
 
-console.log( parseInt(fontSize, 10) );
+// console.log( parseInt(fontSize, 10) );
 
-let answer = +prompt('Hello', '');
-console.log(typeof( parseInt(answer) ));
+// let answer = +prompt('Hello', '');
+// console.log(typeof( parseInt(answer) ));
 
-// toBoolean
-// 0, '', null, undefined, NaN;
-// 1)
-let sw = null;
-sw  = 1;
-if (sw) {
-    console.log('Working...')
-}
+// // toBoolean
+// // 0, '', null, undefined, NaN;
+// // 1)
+// let sw = null;
+// sw  = 1;
+// if (sw) {
+//     console.log('Working...')
+// }
 
-// 2)
-console.log(typeof(Boolean('4')));
+// // 2)
+// console.log(typeof(Boolean('4')));
 
 // 3)
 console.log(typeof(!!'4'));
