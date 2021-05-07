4c87f73a0a93c11af1ad8b75c985f24f6af8cd3f
4i! aka /bin/bashrrY <abrakadabra21099@gmail.com>
2021-05-07 17:28:51 +0400

    Learn 69. Done.

--- js/script.js
+++ js/script.js
@@ -54,3 +54,51 @@ const summa = {
 console.log(summa.summa);
 summa.prA = 20;
 console.log(summa.summa);
+
+// function Abc(a, b, c) {
+//     this.a = a;
+//     this.b = b;
+//     this.c = c;
+
+//     let private = `${a} + ${b} + ${c}`;
+
+//     this.concat = function() {
+//         console.log(`=- ${this.a} + ${this.b} + ${this.c} -=`);
+//     };
+// }
+
+class Abc {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this._priv = `${a} + ${b} + ${c}`;
+    }
+
+    #secret = 888;
+
+    concat() {
+        console.log(`=- ${this.a} + ${this.b} + ${this.c} -=`);
+        console.log(this._priv);
+    }
+    get propA() {
+        return `=-${this.a}-= ${this.#secret}`;
+    }
+    set propA(newA) {
+        if (typeof newA === 'number') {
+            this.a += newA;
+        } else {
+            console.log(`${newA} - bad type.`)
+        }
+    }
+    get pubSecret() {
+        return this.#secret
+    }
+    set pubSecret(newSecret) {
+        this.#secret = newSecret;
+    }
+}
+
+const obj = new Abc('aa', 'bbb', 7777);
+obj.pubSecret = 'super secret';
+console.log(obj.pubSecret);
